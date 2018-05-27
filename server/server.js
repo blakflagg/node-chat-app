44f47d2ba612d5eff582278445d09c30601ba3f7
blakflagg <dduffy2@outlook.com>
2018-05-27 10:11:32 -0700

    lesson 107 Adding Socket.io to the App

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname,'../public');
+const socketIO = require('socket.io');
+
 
 const express = require('express');
-const app = express();
 const port = 3000;
+var app = express();
+var server = http.createServer((app)); //http.createServer required for socket.io
+var io = socketIO(server);
+
 
 app.use(express.static(publicPath));
 
-app.listen(port,() => {
+io.on('connection',(socket) => {      //event occurs when a client connects to the server
+  console.log('new user connected');
+
+  socket.on('disconnect',() => {      //event occurs when client disconnects
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port,() => {
   console.log(`Server is up on ${port}`);
 });
