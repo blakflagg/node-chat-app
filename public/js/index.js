9d3227a758c477b0e3d7a8b28c57a04557c29738
blakflagg <dduffy2@outlook.com>
2018-05-28 10:34:10 -0700

    lessons 110 - 113 setup broadcast; created a message generator; created a message form

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,6 @@
 var socket = io();                    //intiates request from client to the server
 socket.on('connect', function() {           //built-in event occurs when a connection is made to the server
   console.log('connected to server'); 
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    text: 'hello'
-  });
 });
 
 socket.on('disconnect', function() {       //built-in event occurs when server connection is lost
@@ -13,9 +9,21 @@ socket.on('disconnect', function() {       //built-in event occurs when server c
 
 socket.on('newMessage', function(message){
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
 });
 
 socket.on('tick', function(count){
   console.log(count);
 });
 
+jQuery('#message-form').on('submit', function(e){
+  e.preventDefault();
+  socket.emit('createMessage',{
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  },function() {
+
+  });
+});
--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const socketIO = require('socket.io');
 const events = require('events');
 const eventEmitter = new events.EventEmitter();
 
+const {generateMessage} = require('./utils/message');
+
 
 const express = require('express');
 const port = 3000;
@@ -12,21 +14,18 @@ var app = express();
 var server = http.createServer((app)); //http.createServer required for socket.io
 var io = socketIO(server);
 
-
-
 app.use(express.static(publicPath));
-
 
 io.on('connection',(socket) => {      //event occurs when a client connects to the server
   console.log('new user connected');
 
-  socket.emit('newMessage', {
-    from: 'jon',
-    text: 'yo',
-    createdAt: 123
-  });
-  socket.on('createMessage', (message) => {
+  socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
+  socket.broadcast.emit('newMessage',generateMessage('Admin','New User Joined'));
+  
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
+    io.emit('newMessage',generateMessage(message.from,message.text));
+    callback();
   })
   socket.on('disconnect',() => {      //event occurs when client disconnects
     console.log('User was disconnected');
