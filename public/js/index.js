4623f96b61aa485f31c661f7196e00f207481722
blakflagg <dduffy2@outlook.com>
2018-05-28 12:21:09 -0700

    lesson 116 styled the chat app and made the ux better

--- public/js/index.js
+++ public/js/index.js
@@ -32,11 +32,12 @@ socket.on('newLocationMessage', function(message){
 
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
+  var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage',{
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   },function() {
-
+    messageTextbox.val('');
   });
 });
 
@@ -46,13 +47,16 @@ locationButton.on('click', function(){
   if(!navigator.geolocation) {
     return alert('Geolocation not supported by your browers');
   }
+  locationButton.attr('disabled','disabled').text("sending location...");
 
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage',{
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   })
 });
